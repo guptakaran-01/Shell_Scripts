4bb628ceaf083c6491a0b2444009d389b7bacd9f
guptakaran-01 <guptakaranmanoj@amityonline.com>
2025-10-27 00:53:20 +0530

    Adding new file

--- Automatic_git_push.sh
+++ Automatic_git_push.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 
+# Making a shell script to commit on github
 
+status =$(git status | awk '/Untracked/{print $1}')
+add= $(git add .)
+commit=$(git commit -m "Adding new file ")
+change=$(git status | awk '/Changes/{print $1}')
+
+if [[ $status -eq "Untracked" ]] || [[ $change -eq "Changes" ]]
+then 
+	echo "Unteracked files"
+	$add
+        $commit
+else
+	exit 1
+
+fi
