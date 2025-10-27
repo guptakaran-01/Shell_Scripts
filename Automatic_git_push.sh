4de908a74952a40396d359c541e4e81510e3ee60
guptakaran-01 <guptakaranmanoj@amityonline.com>
2025-10-27 18:51:15 +0530

    Automated commit Mon Oct 27 18:51:15 IST 2025

--- Automatic_git_push.sh
+++ Automatic_git_push.sh
@@ -8,9 +8,10 @@ change=$(git status | awk '/Changes/{print $1}')
 
 if [[ $statuss == "Untracked" ]] || [[ $change == "Changes" ]]
 then 
-	echo "Untracked files"
+	echo "Automated push to github"
 	git add .
 	git commit -m "Automated commit $(date) "
+	git push origin main
 else
 	echo "Nothing to commit"
 	exit 1
